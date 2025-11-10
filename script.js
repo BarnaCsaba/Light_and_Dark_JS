2c2972bbfcd7d5e2de729dfe444895f79aa2f535
BarnaCsaba_PG <barna.csaba@diak.szbi-pg.hu>
2025-11-10 12:38:10 +0100

    c

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
+let t = false
+
 function switchTheme() {
   // TODO: implement toggling the light/dark theme
   document.body.classList.add('bg-dark')
   document.getElementById('navbar')
   const navbar =   document.getElementById('navbar')
 
-  navbar.classList.remove('navbar-light', 'bg-light')
-  navbar.classList.add('navbar-dark', 'bg-dark')
 
   const cards = document.querySelectorAll('.card')
   cards.forEach(card => {
@@ -27,4 +27,24 @@ function switchTheme() {
   footer.style.paddingLeft = "1700px"
   footer.style.display = "block"
   footer.classList.add('text-white')
+
+const h1title = document.getElementById('h1-title')
+h1title.innerHTML = "This is the dark theme"
+h1title.classList.add("text-white")
+
+const light = document.getElementById('themeSwitch')
+light.innerHTML = "Activate Light Theme"
+
+
+if (theme === false)
+{
+
+}
+else
+{
+  navbar.classList.remove('navbar-light', 'bg-light')
+  navbar.classList.add('navbar-dark', 'bg-dark')
+
+}
+theme = !theme
 }
