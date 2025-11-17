76bb46f75cd28ac2d8d1e853d64cb19fae91a712
Barna Csaba <BarnaCsaba@pg.local>
2025-11-17 12:16:46 +0100

    f

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-let t = false
-
+let theme = false
 function switchTheme() {
   // TODO: implement toggling the light/dark theme
-  document.body.classList.add('bg-dark')
+  
   document.getElementById('navbar')
   const navbar =   document.getElementById('navbar')
-
-
   const cards = document.querySelectorAll('.card')
+  const tables = document.querySelectorAll('.table')
+  const h1title = document.getElementById('h1-title')
+  const cardColums = document.querySelectorAll('.column')
+  
+if (theme === false)
+{
+  document.body.classList.add('bg-dark')
+  navbar.classList.remove('navbar-light', 'bg-light')
+  navbar.classList.add('navbar-dark', 'bg-dark')
   cards.forEach(card => {
     card.classList.add('text-white', 'bg-dark', 'border-light')
   })
 
 
-  const tables = document.querySelectorAll('.table')
   tables.forEach(table => {
     table.classList.add('table-dark')
   })
 
-  const cardColums = document.querySelectorAll('.column')
   cardColums.forEach(card => {
     card.classList.remove('col')
   })
@@ -28,22 +32,51 @@ function switchTheme() {
   footer.style.display = "block"
   footer.classList.add('text-white')
 
-const h1title = document.getElementById('h1-title')
 h1title.innerHTML = "This is the dark theme"
 h1title.classList.add("text-white")
 
 const light = document.getElementById('themeSwitch')
 light.innerHTML = "Activate Light Theme"
 
-
-if (theme === false)
-{
-
 }
 else
 {
+ 
   navbar.classList.remove('navbar-light', 'bg-light')
   navbar.classList.add('navbar-dark', 'bg-dark')
+
+  document.body.classList.remove("bg-dark")
+  document.body.classList.add('bg-light')
+  navbar.classList.remove('navbar-dark', 'bg-dark')
+  navbar.classList.add('navbar-light', 'bg-light')
+  cards.forEach(card => {
+
+    card.classList.remove('text-white', 'bg-dark', 'border-dark')
+    card.classList.add('text-black', 'bg-light', 'border-dark')
+  })
+
+
+  tables.forEach(table => {
+    table.classList.remove('table-dark')
+    table.classList.add('table-light')
+  })
+
+  cardColums.forEach(card => {
+    card.classList.remove('col')
+  })
+
+  const footer = document.querySelector('.navbar-text')
+  footer.style.paddingLeft = "1700px"
+  footer.style.display = "block"
+  footer.classList.add('text-white')
+
+h1title.innerHTML = "This is the light theme"
+
+h1title.classList.add("text-black")
+
+const light = document.getElementById('themeSwitch')
+light.innerHTML = "Activate Dark Theme"
+
 
 }
 theme = !theme
